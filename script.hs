d5c61a6d3b8300f0810a94770b86060afbf8f687
Vo Minh Thu <thu@hypered.io>
2021-03-27 21:24:05 +0100

    Add Cond.

--- script.hs
+++ script.hs
@@ -21,7 +21,14 @@ main = do
 
     -- Evaluate one rule, but also provide an input
     "--set" : var : val : [name] -> do
-      print $ evaluate name rules [Input var (Int $ read val)]
+      case val of
+        _ | all (`elem` "0123456789") val ->
+          print $ evaluate name rules [Input var (Int $ read val)]
+        "True" ->
+          print $ evaluate name rules [Input var (Bool True)]
+        "False" ->
+          print $ evaluate name rules [Input var (Bool False)]
+        _ -> error "TODO"
 
     _ -> error "TODO Usage."
 
@@ -30,6 +37,7 @@ main = do
 evaluate name rs is = case filter ((== name) . rName) rs of
   [r] -> case rFormula r of
     Unset -> case lookupInput (rName r) is of
+      Just (Bool x) -> Result (Bool x)
       Just (Int x) -> Result (Int x)
       Just _ -> error "Inputs cannot contain Unset or Name."
       Nothing -> UnsetVariables [rName r]
@@ -41,6 +49,12 @@ reduce e rs is = case e of
   Bool x -> Result (Bool x)
   Int x -> Result (Int x)
   Name name -> evaluate name rs is
+  Cond e1 e2 e3 -> case reduce e1 rs is of
+    (Result (Bool True)) -> reduce e2 rs is
+    (Result (Bool False)) -> reduce e3 rs is
+    (Result t) -> Error (TypeMismatch $ "Expected a Bool, got " ++ show t)
+    Error err -> Error err
+    UnsetVariables xs -> UnsetVariables xs
   Add e1 e2 -> case (reduce e1 rs is, reduce e2 rs is) of
     (Result (Int a), Result (Int b)) -> Result (Int (a + b))
     (Result (Int _), Result t) -> Error (TypeMismatch $ "Expected an Int, got " ++ show t)
@@ -76,7 +90,13 @@ data Formula = Unset | Exp Exp
   deriving (Eq, Show)
 
 -- An expression can be a literal, or the use of a rule, or an addition.
-data Exp = Bool Bool | Int Int | Name String | Add Exp Exp | Sum [Exp]
+data Exp =
+    Bool Bool
+  | Int Int
+  | Name String
+  | Cond Exp Exp Exp -- if _ then _ else _
+  | Add Exp Exp
+  | Sum [Exp]
   deriving (Eq, Show)
 
 data EvaluationError = NoSuchRule | MultipleRules | Cycles | TypeMismatch String
@@ -115,11 +135,21 @@ tests =
   , evaluate "i" [Rule "i" (Exp $ Bool True)] [] == Result (Bool True)
   , isTypeMismatch $ evaluate "j" [Rule "j" (Exp $ Add (Int 1) (Bool True))] []
   , isTypeMismatch $ evaluate "j" [Rule "j" (Exp $ Add (Bool True) (Int 1))] []
+  , evaluate "k" [Rule "k" (Exp $ Cond (Bool True) (Int 1) (Int 2))] []
+      == Result (Int 1)
+  , isTypeMismatch $ evaluate "j" [Rule "j" (Exp $ Cond (Int 0) (Int 1) (Int 2))] []
+  , evaluate "l" rules [] == UnsetVariables ["i"]
+  , evaluate "l" rules [Input "i" (Bool True)] == Result (Int 5)
+  , evaluate "l" rules [Input "i" (Bool False)] == UnsetVariables ["e"]
+  , evaluate "l" rules [Input "i" (Bool False), Input "e" (Int 4)] == Result (Int 4)
   ] 
 
 --------------------------------------------------------------------------------
 -- Examples rules to play with the CLI.
-rules = [rule_1, rule_2, rule_3, rule_4, rule_5, rule_6, rule_7, rule_8, rule_cycle]
+rules =
+  [ rule_1, rule_2, rule_3, rule_4, rule_5, rule_6, rule_7, rule_8, rule_9
+  , rule_10, rule_11, rule_cycle
+  ]
 
 rule_1 = Rule "a" (Exp (Int 5))
 
@@ -137,5 +167,11 @@ rule_6 = Rule "f" (Exp (Name "e"))
 rule_7 = Rule "g" (Exp (Add (Name "a") (Name "b")))
 
 rule_8 = Rule "h" (Exp (Sum [Name "a", Name "b", Name "c"]))
+
+rule_9 = Rule "i" Unset
+
+rule_10 = Rule "k" (Exp (Cond (Name "i") (Int 1) (Int 2)))
+
+rule_11 = Rule "l" (Exp (Cond (Name "i") (Name "a") (Name "e")))
 
 rule_cycle = Rule "cycle" (Exp (Name "cycle")) -- TODO Find cycles.
