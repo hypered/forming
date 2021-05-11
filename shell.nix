f5cb912048b1868dc43f31221315b6dfaea442f9
Vo Minh Thu <thu@hypered.io>
2021-05-11 11:30:27 +0200

    Add a .cabal file.

--- shell.nix
+++ shell.nix
@@ -1,14 +1,3 @@
 { pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    buildInputs = [
-      pkgs.ghcid
-      (pkgs.haskellPackages.ghcWithPackages (hpkgs: [
-        hpkgs.aeson
-        hpkgs.blaze-html
-        hpkgs.lens
-        hpkgs.snap
-        hpkgs.snap-core
-        hpkgs.snap-server
-      ]))
-    ];
-}
+
+(pkgs.haskellPackages.callCabal2nix "forming" ./. { }).env
