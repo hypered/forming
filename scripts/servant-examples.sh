2d9ed6fee6d2f073d0170441fda1954800408661
Vo Minh Thu <thu@hypered.io>
2023-07-14 13:22:42 +0200

    Add a helper script.
    This runs the Servant -based server (instead of the Snap one).

---
+++ scripts/servant-examples.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+echo Visit http://127.0.0.1:9000/noteed/.
+echo
+
+export FORMING_SITE_DIR=../design
+runghc -i../design/src -i../syntactical \
+  -XOverloadedStrings \
+  -XTypeApplications \
+  bin/forming-examples.hs servant
